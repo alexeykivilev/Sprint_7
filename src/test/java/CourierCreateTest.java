9489dc131b36b96ad4b8c5d74486aa6db6ae49b0
Alexey Kivilev <alexkivi10@yandex.ru>
2024-03-07 16:40:08 +0500

    edit test structure, add tearDown method

--- src/test/java/CourierCreateTest.java
+++ src/test/java/CourierCreateTest.java
@@ -3,33 +3,35 @@ import io.qameta.allure.junit4.DisplayName;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class CourierCreateTest {
-    private String login = RandomStringUtils.randomAlphabetic(10);;
-    private String password = RandomStringUtils.randomNumeric(10);;
-    private String firstName = RandomStringUtils.randomAlphabetic(10);;
+    private String login = RandomStringUtils.randomAlphabetic(10);
+    ;
+    private String password = RandomStringUtils.randomNumeric(10);
+    ;
+    private String firstName = RandomStringUtils.randomAlphabetic(10);
+    ;
+    private String courierId;
 
 
     @Before
     public void setUp() {
         RestAssured.baseURI = "https://qa-scooter.praktikum-services.ru";
     }
+
     NewCourier newCourier = new NewCourier(login, password, firstName);
 
     @Test
-    @DisplayName("Creating new courier test")
-    public void createNewCourier() {
-        Response response = createNewCourierTest();
-        Response response2 = duplicateCourierTest();
-
-    }
-
-    @Step("Send POST request to /api/v1/courier")
-    public Response createNewCourierTest() {
+    @DisplayName("Send POST request to /api/v1/courier")
+    public void createNewCourierTest() {
         Response response =
                 given()
                         .header("Content-type", "application/json")
@@ -38,32 +40,27 @@ public class CourierCreateTest {
                         .when()
                         .post("/api/v1/courier");
         response.then().statusCode(201).assertThat().body("ok", equalTo(true));
-        return response;
-    }
-
-    @Step("Cannot create duplicate courier")
-    public Response duplicateCourierTest() {
-        Response response2 =
-                given()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(newCourier)
-                        .when()
-                        .post("/api/v1/courier");
-        response2.then().statusCode(409);
-        return response2;
+        courierId = response.jsonPath().getString("id");
     }
 
     @Test
-    @DisplayName("Missing fields test")
-    public void missingFieldsTest() {
-        Response response = missingLoginFieldTest();
-        Response response1 = missingPasswordFieldTest();
+    @DisplayName("Cannot create duplicate courier")
+    public void duplicateCourierTest() {
+        File json = new File("src/test/resources/duplicateCourier.json");
+        Response response =
+                given()
+                        .header("Content-type", "application/json")
+                        .and()
+                        .body(json)
+                        .when()
+                        .post("/api/v1/courier");
+        response.then().statusCode(409);
     }
 
-    @Step("Missing login field")
-    public Response missingLoginFieldTest() {
-        NewCourier newCourier = new NewCourier(null, "123456", "ivano");
+    @Test
+    @DisplayName("Missing login field")
+    public void missingLoginFieldTest() {
+        NewCourier newCourier = new NewCourier(null, password, firstName);
         Response response =
                 given()
                         .header("Content-type", "application/json")
@@ -72,20 +69,30 @@ public class CourierCreateTest {
                         .when()
                         .post("/api/v1/courier");
         response.then().body("message", equalTo("Недостаточно данных для создания учетной записи"));
-        return response;
     }
 
-    @Step("Missing password field")
-    public Response missingPasswordFieldTest() {
-        NewCourier newCourier = new NewCourier("test12343asdads", null, "ivano");
-        Response response1 =
+    @Test
+    @DisplayName("Missing password field")
+    public void missingPasswordFieldTest() {
+        NewCourier newCourier = new NewCourier(login, null, firstName);
+        Response response =
                 given()
                         .header("Content-type", "application/json")
                         .and()
                         .body(newCourier)
                         .when()
                         .post("/api/v1/courier");
-        response1.then().body("message", equalTo("Недостаточно данных для создания учетной записи"));
-        return response1;
+        response.then().body("message", equalTo("Недостаточно данных для создания учетной записи"));
+    }
+
+    @After
+    public void tearDown() {
+        if (courierId != null) {
+            Response response =
+                    given()
+                            .when()
+                            .delete("/api/v1/courier/" + courierId);
+            response.then().statusCode(200).assertThat().body("ok", equalTo(true));
+        }
     }
 }
